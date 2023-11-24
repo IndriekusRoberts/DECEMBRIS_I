cc8107b52f8a15da44bab38cef7c37e77f3adfa2
IndriekusRoberts <129832170+IndriekusRoberts@users.noreply.github.com>
2023-11-24 10:43:10 +0200

    Delete 24_nov/DECEMBRIS_I directory

--- 24_nov/DECEMBRIS_I/2uzd.py
+++
@@ -1,5 +0,0 @@
-number = 1
-while number ** 2 <= 1000:
-    number += 1
-
-print(number)
--- 24_nov/DECEMBRIS_I/4uzd.py
+++
@@ -1,9 +0,0 @@
-def factorial(n): 
-    bing = 1
-    for i in range(2, n+1): 
-        bing *= i 
-    return bing
-   
-number = 4; 
-print("Factorial skaitlim", number, "ir", 
-factorial(number)) 
